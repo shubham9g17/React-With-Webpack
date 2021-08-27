5a2d85c1809f07b78cf738705d8a6d3e99fc0290
shubham9gupta19 <shubham9gupta19@gmail.com>
2021-08-27 18:24:12 +0530

    css , image , svg file support added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-const App = () => {
-  return <h1>Hello React</h1>;
+import "./styles.css";
+import Image from "./react.png";
+import Logo from "./redux.svg";
+export const App = () => {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <img src={Image} alt="React Logo" />
+      <img src={Logo} alt="Redux Logo" />
+    </>
+  );
 };
-export default App;
